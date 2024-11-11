c4399020aadb4b6ab8c27886f831a2241c4d92c8
mark <hung.nguyen2@employmenthero.com>
2024-11-11 23:37:29 +0700

    chore:  inconsistency  "no-cond-assign": ["error", "always"]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
   extends: "eslint:recommended",
-  plugins: ["jest"],
   parserOptions: {
     ecmaVersion: 6, // default is 5
   },
   env: {
     node: true,
-    "jest/globals": true,
   },
   rules: {
-    "jest/expect-expect": "error",
+    "no-undef": ["error"],
+    "no-cond-assign": ["error", "always"],
   },
 };
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const a = 1;
+const numbers = [1, 2, 3, 4, 5];
 
-console.log(a);
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i]);
+}
