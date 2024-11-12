f96e6406003b4d7a75e5d3de5a19a3e5ddd57a46
mark <hung.nguyen2@employmenthero.com>
2024-11-12 08:41:18 +0700

    reset main branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,11 @@
 module.exports = {
   extends: "eslint:recommended",
-  plugins: ["jest"],
+  plugins: [],
   parserOptions: {
     ecmaVersion: 6, // default is 5
   },
   env: {
     node: true,
-    "jest/globals": true,
   },
-  rules: {
-    "jest/expect-expect": "error",
-  },
+  rules: {},
 };
